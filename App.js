087785b9f5fed565c473cb7442444952bd938125
Franz <franzcmarcelo@gmail.com>
2022-10-19 01:39:31 -0500

    RandomGif component with sharing

--- App.js
+++ App.js
@@ -5,15 +5,37 @@ import {
   StyleSheet,
   Image,
   Button,
+  TouchableOpacity,
 } from "react-native";
+import * as Sharing from 'expo-sharing';
 
 const App = () => {
 
   const [image, setImage] = useState(null);
+  const [gif, setGif] = useState(null);
 
   const getRamdomImage = async () => {
     const getRandomInt = max => Math.floor(Math.random() * max);
     setImage(`https://picsum.photos/200?random=${getRandomInt(100)}`);
+  }
+
+  const getRamdomGif = async () => {
+    const response = await fetch('https://api.giphy.com/v1/gifs/random?api_key=8sL1GObqbeNMOFgHLjAAT5YNniSwvYHb');
+    console.log(response);
+    const responseJson = await response.json();
+    const { data } = responseJson;
+    const { images } = data;
+    const { original } = images;
+    const { url } = original;
+    setGif(url);
+  }
+
+  const shareGif = async () => {
+    if(!(await Sharing.isAvailableAsync())) {
+      alert(`Uh oh, sharing isn't available on your platform`);
+      return;
+    }
+    await Sharing.shareAsync(gif);
   }
 
   return (
@@ -26,6 +48,21 @@ const App = () => {
       <Button color="#000" title="Get ramdom image" onPress={getRamdomImage} />
       {
         image && <Image source={{ uri: image }} style={styles.image} />
+      }
+
+      {/* RAMDOM GIF */}
+      <TouchableOpacity style={styles.touchable} onPress={getRamdomGif}>
+        <Text style={styles.touchableText}>Get Ramdom Gif</Text>
+      </TouchableOpacity>
+      {
+        gif && (
+          <>
+            <Image source={{ uri: gif }} style={styles.image} />
+            <TouchableOpacity style={styles.touchable} onPress={shareGif}>
+              <Text style={styles.touchableText}>Sharing gif</Text>
+            </TouchableOpacity>
+          </>
+        )
       }
 
     </View>
