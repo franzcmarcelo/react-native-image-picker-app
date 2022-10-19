04a63a6555712ad51a5adabe339206c10cb5176d
Franz <franzcmarcelo@gmail.com>
2022-10-19 01:36:24 -0500

    RamdomImage component

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Button,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+
+  const [image, setImage] = useState(null);
+
+  const getRamdomImage = async () => {
+    const getRandomInt = max => Math.floor(Math.random() * max);
+    setImage(`https://picsum.photos/200?random=${getRandomInt(100)}`);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.title}>My first app</Text>
+
+
+      {/* RAMDOM IMAGE */}
+      <Button color="#000" title="Get ramdom image" onPress={getRamdomImage} />
+      {
+        image && <Image source={{ uri: image }} style={styles.image} />
+      }
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    gap: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#27379b",
+    paddingTop: 20,
+    paddingBottom: 20,
   },
+  title: {
+    fontSize: 30,
+    color: "white"
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 20
+  },
+  touchable: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 10,
+  },
+  touchableText: {
+    color: "#000",
+    fontSize: 16,
+  }
 });
+
+export default App;
